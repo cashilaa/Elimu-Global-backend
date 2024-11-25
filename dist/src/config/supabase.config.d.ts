df45d304498a05700039546be1a54d8521b4cda4
sheila wafula <167983359+cashilaa@users.noreply.github.com>
2024-11-25 18:10:07 +0300

    Delete dist directory

--- dist/src/config/supabase.config.d.ts
+++
@@ -1,2 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare const createSupabaseClient: (configService: ConfigService) => import("@supabase/supabase-js").SupabaseClient<any, "public", any>;
--- dist/src/enums/user-role.enum.d.ts
+++
@@ -1,5 +0,0 @@
-export declare enum UserRole {
-    ADMIN = "admin",
-    STUDENT = "student",
-    INSTRUCTOR = "instructor"
-}
