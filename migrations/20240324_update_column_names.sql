c8b70c4527c30936fbc18b3e27e77429dd2f1a1b
sheila wafula <167983359+cashilaa@users.noreply.github.com>
2024-11-25 18:10:18 +0300

    Delete migrations directory

--- migrations/20240324_update_column_names.sql
+++
@@ -1,5 +0,0 @@
--- Rename columns in users table to use camelCase
-ALTER TABLE users 
-  RENAME COLUMN is_approved TO "isApproved",
-  RENAME COLUMN created_at TO "createdAt",
-  RENAME COLUMN updated_at TO "updatedAt";
