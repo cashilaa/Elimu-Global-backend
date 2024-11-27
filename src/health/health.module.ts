dfad95de6655f44f0cf12798bb368784aabce544
cashilaa <wafulasheila26@gmail.com>
2024-11-27 14:58:47 +0300

    chore: remove backend codebase

--- src/health/health.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthController } from './health.controller';
-import { SupabaseModule } from '../supabase/supabase.module';
-
-@Module({
-  imports: [SupabaseModule],
-  controllers: [HealthController],
-})
-export class HealthModule {}
